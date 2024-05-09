cfc69f53f8338925c6dbb1125d8a995eed42e2ec
hassan <kkromah12@gmail.com>
2024-05-09 16:40:44 +0300

    edited

--- src/LunchStr4.java
+++ src/LunchStr4.java
@@ -3,13 +3,24 @@ public class LunchStr4 {
 
 	public static void main(String[] args) {
 		// Concatenation
+		String s1 = "Telusko" + " Java";// Can add any amount of objects
+		System.out.println(s1);
+
 		String str1 = "Telusko";
 		System.out.println(str1.concat(" Ahmet"));
 
 		String str2 = "Telusko";
-		str2.concat("Java");
+		str2.concat("Java");// can add only 1 object
 		System.out.println(str2);
 
-	}
+		String s2 = "100" + 4 + 10;
+		String s3 = 100 + 4 + "10";
+		System.out.println("s2:" + s2 + "; " + "s3:" + s3);
+		
+		double d = 20.3;
+		double d1= 6.5;
+		//System.out.println(d.cocat(d1)); can not concat primitive type double.
+			
 
+	}
 }
